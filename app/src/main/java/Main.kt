8a9f40f447bd9dd2a725e1274b70820f278e6e0c
habejo <abejohanna@gmail.com>
2021-05-11 17:12:42 +0800

    Add conditions if and when

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -16,5 +16,35 @@ fun main(args: Array<String>) {
     val years: Double = weeks / 52.0
     println("$weeks weeks is $years years ")
 
+    println()
+
+    conditions()
+
+}
+
+
+fun conditions() {
+    val lives = 3
+
+    val isGameOver = (lives<1)
+    println("Game Over? $isGameOver")
+
+    if (isGameOver) {
+        println("Game over!")
+    } else {
+        println("You're still alive!")
+    }
+
+    println("How old are you? ")
+    val age = readLine()!!.toInt()
+    println("age is $age")
+
+    val message = when {
+        age < 17 -> "You're too young to vote!"
+        age > 60 -> "You're too old to vote!"
+        else -> "Congratulations! You can vote."
+    }
+
+    println(message)
 
 }
