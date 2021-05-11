5abafbea254129c69714bb29a415f1eef947f46f
habejo <abejohanna@gmail.com>
2021-05-11 16:51:00 +0800

    Variables and Strings

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
     println("Hello World")
+    println("This is the main function")
+
+    var name: String = "Hanna"
+    println(name)
+
+    var monthlySalary: Int = 10
+    var annualSalary: Int = monthlySalary * 12
+    println("Monthly salary: $monthlySalary")
+    println("Annual salary : $annualSalary")
+
+    println();
+
+    val weeks: Int = 234
+    val years: Double = weeks / 52.0
+    println("$weeks weeks is $years years ")
+
+
 }
